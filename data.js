1f4aae7e065a958b99be7309ee9b921b6df0a8bc
Som Bohora <som-bohora@ouhsc.edu>
2018-12-16 01:03:05 -0600

    update quiz app

---
+++ data.js
@@ -0,0 +1,148 @@
+const DATASTORE = [
+    {
+        number: 1,        
+        question: 'What is the highest mountain in the world?',
+        choices: [
+            "Mt. Everest",
+            "Mt. K2",
+            "Mt. Chomo Lonzo",
+            "Mt. Dhaulagiri II"
+        ],
+        correctAnswer: 'Mt. Everest',
+        image: 'img/everest.png',
+        alt: 'everest'
+    },
+    {
+        number: 2,
+        question: 'Which country is Mt. Everest in, the highest peak of the world?',
+        choices: [
+            'Pakistan',
+            'Nepal',
+            'United States of America',
+            'China'
+        ],
+        correctAnswer: 'Nepal',
+        image: 'img/nepal.png',
+        alt: 'nepal'
+    },
+    { 
+        number: 3,
+        question: 'Kailas is on which continent?',
+        choices: [
+            'Asia',
+            'South-America',
+            'North-America',
+            'Antarctica'
+        ],
+        correctAnswer: 'Asia',
+        image: 'img/kailash.png',
+        alt: 'asia'
+    },
+    {
+        number: 4,
+        question: 'Mount Logan - Yukon is in which country/countries?',
+        choices: [
+            'United States',
+            'Russia',
+            'Georgia',
+            'Canada'
+        ],
+        correctAnswer: 'Canada',
+        image: 'img/logan.png',
+        alt: 'canada'
+    },
+    {
+        number: 5,
+        question: 'Shishapangma is in which country/countries?',
+        choices: [
+            'Russia',
+            'Turkey',
+            'Romania',
+            'China'
+        ],
+        correctAnswer: 'China',
+        image: 'img/shishapangma.png',
+        alt: 'china'
+    },
+    {
+        number: 6,
+        question: 'The mountain Annapurna is in which country/countries?',
+        choices: [
+            'Japan',
+            'India',
+            'Nepal',
+            'Indonesia'
+        ],
+        correctAnswer: 'Nepal',
+        image: 'img/annapurna.png',
+        alt: 'nepalannapurna'
+    },
+    {
+        number: 7,
+        question: 'Mount McKinley is in which country/countries?',
+        choices: [
+            'Philippines',
+            'United States',
+            'Canada',
+            'Venezuela'
+        ],
+        correctAnswer: 'United States',
+        image:  'img/mckinley.png',
+        alt: 'usa'
+    },
+    {
+        number: 8,
+        question: 'Table Mountain is in which country/countries?',
+        choices: [
+            'Morocco',
+            'South-Africa',
+            'Algeria',
+            'Kenya'
+        ],
+        correctAnswer: 'South-Africa',
+        image: 'img/table-mountain.png',
+        alt: 'southafrica'
+    },
+    {
+        number: 9,
+        question: 'Bishorn is in which country/countries?',
+        choices: [        
+           'Austria alone',
+           'Switzerland alone',
+           'Germany',
+           'Poland'
+        ],
+        correctAnswer: 'Switzerland alone',
+        image: 'img/bishorn.png',
+        alt: 'switzerland'
+    },
+    {
+        number: 10,
+        question: 'Mauna Kea is in which country/countries?',
+        choices: [        
+            'United States',
+            'Japan',
+            'Philippines',
+            'Indonesia'
+        ],
+        correctAnswer: 'United States',
+        image: 'img/maunakea.png',
+        alt: 'usa'
+    },
+
+   
+
+    
+    
+   
+
+    
+  
+
+    
+    
+
+    
+
+]
+
---
+++ index.js
@@ -0,0 +1,148 @@
+questionIndex = 0;
+score = 0;
+
+correctAnswers = 0;
+
+function generateQuestionForm() {
+    return `
+    <section id="question-section" role="main">
+        <h1 id="question">${DATASTORE[questionIndex].question}</h2>
+        <form>
+            <fieldset>
+                <div>
+                    <input type="radio" class='q1' name="q1" value = "${DATASTORE[questionIndex].choices[0]}" checked>
+                    <span>${DATASTORE[questionIndex].choices[0]}</span><br>
+                    <input type="radio" class='q1' name="q1" value = "${DATASTORE[questionIndex].choices[1]}">
+                    <span>${DATASTORE[questionIndex].choices[1]}</span><br>
+                    <input type="radio" class='q1' name="q1" value = "${DATASTORE[questionIndex].choices[2]}">
+                    <span">${DATASTORE[questionIndex].choices[2]}</span><br>
+                    <input type="radio" class='q1' name="q1" value = "${DATASTORE[questionIndex].choices[3]}">
+                    <span>${DATASTORE[questionIndex].choices[3]}</span>
+                    </div>
+            </fieldset>
+            <button id="js-btn-submit">Submit</button>
+        </form>
+        
+        <div>
+            <span id="count-questions">Quesion #: ${DATASTORE[questionIndex].number}</span>
+            <span id="count-score">Score: ${correctAnswers}/${DATASTORE.length} </span>
+            
+        </div>
+    </section>`
+};
+
+function handleStartButton() {
+    $('#btn-start').on('click', function(e){
+        nextquizQuestions();
+    })
+}
+
+function nextquizQuestions() {
+    $('#container').html(generateQuestionForm())
+}
+
+function handleSubmitButton(){
+    $('#container').on('click', '#js-btn-submit', function(e){
+        e.preventDefault();
+
+        const getUserAnswer = $('input:checked').val();
+        console.log(getUserAnswer);
+
+        let isUserCorrect;
+        if (getUserAnswer ==  DATASTORE[questionIndex].correctAnswer) {
+            isUserCorrect = true;
+        } else {
+            isUserCorrect = false;            
+        }
+        console.log(isUserCorrect);
+        
+        if(isUserCorrect) {
+            showCorrectFeedback();
+          } else {
+            showIncorrectFeedback();
+          }
+
+    
+    })
+};
+
+// function name(params) {
+    
+// }
+
+function templateCorrectFeedback(pic) {
+    return `
+    <section class="correct-feedback-page" role="main">
+    <h2>That's Correct!</h2>
+    <img src="${pic}" class = "resize" alt="Everest."><br>
+    <button id="js-next-button">Next</button>
+    </section>
+    `;
+}
+
+function templateIncorrectFeedback(pic, correct) {
+    return `
+    <section class="incorrect-feedback-page" role="main">
+    <h2>Nope. Correct Answer: ${DATASTORE[questionIndex].correctAnswer}</h2>
+    <img src="${pic}" class = "resize" alt="Everest."><br>
+    <button id="js-next-button">Next</button>
+    </section>
+    `;
+}
+
+function showCorrectFeedback() {
+    $('#container').html(templateCorrectFeedback(DATASTORE[questionIndex].image));
+    correctAnswers++;    
+}
+
+function showIncorrectFeedback() {
+    $('#container').html(templateIncorrectFeedback(DATASTORE[questionIndex].image, DATASTORE[questionIndex].correctAnswer));
+}
+
+
+function renderQuestionsHtml(){
+
+}
+
+function generateFinalScorePage(correctAnswers) {
+    $('#container').html(`
+      <section id="final-page">
+        <h2>Your Final Score: ${correctAnswers} out of 10</h2>
+        <button id="js-restart-button">Play Again?</button>
+      </section>
+    `);
+  }
+
+function handleNextButton() {
+$('#container').on('click', '#js-next-button', function(event) {
+
+    if(questionIndex === 9) {
+        generateFinalScorePage(correctAnswers);
+    } else {
+    nextquizQuestions();
+    questionIndex++;    
+}
+});
+}
+
+function handleRestartButton() {
+    $('#container').on('click', '#js-restart-button', function(event) {
+  
+      questionIndex = 1;
+  
+      correctAnswers = 0;
+  
+      nextquizQuestions();
+    });
+  }
+
+
+
+function main(params) {
+    handleStartButton();
+    handleSubmitButton();
+    handleNextButton();
+    handleRestartButton();
+}
+
+$(main)
